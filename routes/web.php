72e226a0331316469fb6dc0795d1d28a851e3c4f
Allan Almeida <allan.almeida.dev@gmail.com>
2025-06-27 17:47:34 -0300

    add conf do redis e modificacoes do dockerfile e docker-compose

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/publish', function () {
+    // ...
+ 
+    Redis::publish('test-channel', json_encode([
+        'name' => 'Adam Wathan'
+    ]));
 });
